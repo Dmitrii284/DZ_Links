e0227c2e07f3abb7ec437a436ac9ef0cdc4ba3dc
Дмитрий <@ADMIN>
2023-04-03 01:10:22 +0500

    22

--- Links/Source.cpp
+++ Links/Source.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+//Task 1
 void three_max(int& F, int& G, int& X) {
 	int max = F;
 	max = F > G ? F : G;
@@ -7,24 +7,20 @@ void three_max(int& F, int& G, int& X) {
 	F = G = X = max;
 	std::cout << F << ' ' << G << ' ' << X;
 }
-
-int& nega_el(int arr[], const int length) {
-	int b = arr[0];
+//Task 2
+int& nega_el(int arr[], const int length) {	
 	for (int i = 0; i < length; i++)
 		if (arr[i] < 0)
-			return arr[i];
-	
-		else
-			return	arr[0];
-	//return arr[b];
+			return arr[i];	
+	return arr[0];
 }
 
 
 int main() {
+	//Task 1
 	int A = 3, B = 5, C = 4;
 	three_max(A, B, C);
-
-
+	//Task 2
 	int const size = 5;
 	int array[size]{ 222, 5, -10, 8, 20 };
 	std::cout << '\n' << nega_el(array, size)<<std::endl;
