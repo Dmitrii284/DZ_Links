7371d843f89079fd23fa130ffc8c026e9d3aae8f
Дмитрий <@ADMIN>
2023-04-03 17:41:22 +0500

    22

--- Links/Source.cpp
+++ Links/Source.cpp
@@ -14,16 +14,59 @@ int& nega_el(int arr[], const int length) {
 			return arr[i];	
 	return arr[0];
 }
+void rf (int* mass1, int* mass2, const int length1, const int length2) {
+	std::cout << '[';
+	for (int i = 0; i < length1; i++) 
+		std::cout << *(mass1 + i)<<", ";	
+	std::cout << "\b\b]\n"<<std::endl;
+	
+	std::cout << '[';
+	for (int j = 0; j < length2; j++) 
+		std::cout << *(mass2 + j)<<", ";	
+	std::cout << "\b\b]\n" << std::endl;
+
+	if (*mass1 == *mass2)
+		mass2 = 0;
+	for (int j = 0; j < length2; j++)
+	std::cout << *(mass2 + j) << ", ";
+       
+
+	///*int x = 0;
+	//int *px = &x;*/
+	//for (int i = 0; i < length1; i++)
+	//	for (int j = 0; j < length2; j++) {
+	//		if (*mass1 == *mass2)
+	//			mass2 = &x;
+	//		//for (int j = 0; j < length2; j++)
+	//			std::cout << *(mass2 + j) << ", ";
+
+		
+	/**mass2 = *(mass1 + i) == *(mass2 + j) ? *(mass2 + j) : *(mass1 + i);
+		for (int j = 0; j < length2; j++)
+			std::cout << *(mass2 + j) << ", ";*/
+	
+	
+}
 
 
 int main() {
 	//Task 1
 	int A = 3, B = 5, C = 4;
 	three_max(A, B, C);
+
 	//Task 2
 	int const size = 5;
 	int array[size]{ 222, 5, -10, 8, 20 };
-	std::cout << '\n' << nega_el(array, size)<<std::endl;
+	std::cout << '\n' << nega_el(array, size) << std::endl;
+
+	int const size1 = 7;
+	int const size2 = 6;
+	int array1[size1]{ 3, 6, 4, 1, 4, 8, 2 };
+	int array2[size2]{5, 3, 1, 5, 10, 8};
+	rf(array1, array2, size1, size2);
+
+
+
 	
 	return 0;
 }
